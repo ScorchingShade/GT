1397d5bd873d3b62e607e3a6a59b30755a06562d
ScorchingShade <ankushors789@gmail.com>
2018-09-02 00:58:20 +0530

    added firebase login-Email and Password

--- app/src/main/java/com/ankushinc/reddragon/gymtracker/LoginActivity.java
+++ app/src/main/java/com/ankushinc/reddragon/gymtracker/LoginActivity.java
@@ -1,13 +1,85 @@
 package com.ankushinc.reddragon.gymtracker;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    AutoCompleteTextView email;
+    EditText password;
+    Button sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        sign_in=(Button)findViewById(R.id.email_sign_in_button);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if(currentUser!=null){
+           String UserMail=currentUser.getEmail();
+
+            Intent intent=new Intent(getApplicationContext(),tabbed_space.class);
+            startActivity(intent);
+            finish();
+
+        }
+        else
+            Toast.makeText(this,"Login to continue",Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    public void login(View view){
+        email=(AutoCompleteTextView)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+
+        String Email=email.getText().toString().trim();
+        String Password=password.getText().toString().trim();
+
+
+        mAuth.signInWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/com/ankushinc/reddragon/gymtracker/Tab1_TabbedSpace.java
+++ app/src/main/java/com/ankushinc/reddragon/gymtracker/Tab1_TabbedSpace.java
@@ -17,20 +17,43 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
-public class Tab1_TabbedSpace extends Fragment  {
+public class Tab1_TabbedSpace extends Fragment {
     private final String TAG = "Tab1Fragment";
     Context ctx;
     private AdView mAdview;
-    Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
+    Spinner spinner,workout;
+    ArrayAdapter<CharSequence> adapter,adapter1;
     ImageView settings;
     Button btn;
-    AutoCompleteTextView Name,Age,Height,Weight;
+    AutoCompleteTextView Name, Age, Height, Weight,Days;
+    FirebaseAuth auth;
+    FirebaseUser user;
+
+    //LoginButton loginButton;
+
+    CallbackManager callbackManager;
+
+   public Tab1_TabbedSpace(){
+
+    }
+
 
     @Nullable
     @Override
@@ -38,6 +61,11 @@ public class Tab1_TabbedSpace extends Fragment  {
         View view = inflater.inflate(R.layout.tab1_fragment, container, false);
 
         settings = (ImageView) view.findViewById(R.id.settings);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("App Name");
+        myRef.setValue("GymTracker");
+
+
 
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,16 +78,55 @@ public class Tab1_TabbedSpace extends Fragment  {
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner.setAdapter(adapter);
 
+                workout = (Spinner) mView.findViewById(R.id.setting_workout);
+                adapter1 = ArrayAdapter.createFromResource(getActivity(), R.array.workout, android.R.layout.simple_spinner_item);
+                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                workout.setAdapter(adapter1);
+
+                Name = (AutoCompleteTextView) mView.findViewById(R.id.setting_name);
+                Age = (AutoCompleteTextView) mView.findViewById(R.id.setting_age);
+                Height = (AutoCompleteTextView) mView.findViewById(R.id.setting_height);
+                Weight = (AutoCompleteTextView) mView.findViewById(R.id.setting_weight);
+                Days = (AutoCompleteTextView) mView.findViewById(R.id.setting_Goal);
+
+
                 mBuilder.setView(mView);
-                final AlertDialog dialog=mBuilder.create();
+                final AlertDialog dialog = mBuilder.create();
                 dialog.show();
 
-                btn=(Button)mView.findViewById(R.id.setting_done);
+                btn = (Button) mView.findViewById(R.id.setting_done);
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(getActivity(),"We are all set then!",Toast.LENGTH_SHORT).show();
-                        dialog.cancel();
+                        String name = Name.getText().toString();
+                        String gender = spinner.getSelectedItem().toString();
+                        String work = workout.getSelectedItem().toString();
+
+                        try {
+
+
+
+                            String a = Age.getText().toString();
+                            int age = Integer.parseInt(a);
+
+                            String b = Height.getText().toString();
+                            Float height = Float.parseFloat(b);
+
+                            String c = Weight.getText().toString();
+                            int weight = Integer.parseInt(c);
+
+                            String d = Days.getText().toString();
+                            int day = Integer.parseInt(d);
+
+
+                        } catch (Exception e) {
+
+                        } finally {
+                            Toast.makeText(getActivity(), work, Toast.LENGTH_SHORT).show();
+
+                            dialog.cancel();
+                        }
+
                     }
                 });
             }
@@ -69,6 +136,5 @@ public class Tab1_TabbedSpace extends Fragment  {
     }
 
 
-
-    }
+}
 
